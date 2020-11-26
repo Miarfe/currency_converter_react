d0a7294f76e04a20d869c7e7874b9ed4e9abe4b1
Miarfe <efraim.ie@gmail.com>
2020-11-26 21:18:36 +0000

    currency convertion

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
-/*useEffect fetch the API every time the applications is loaded.
-the useState will take it will take the rates descriptions to populate the list of select options from the list.
-*/
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import CurrencyRow from './CurrencyRow';
 
-/** the provided link have stored in JSON format all the latest currency conversions.
-*/
 const BASE_URL = 'https://api.exchangeratesapi.io/latest'
 
 function App() {
-
-  /**The useState hook and the purpose is to state for what the currency list is going to be.
-   * this useState function returns an array of options, the first option is going to be what the
-   * actual state is, the actual current iteration of the current state. The second value is going to be
-   * the object or the function that allows to set the currency option. By default it will pass in an
-   * empty array because there are no options when the app loads.
-   */
   const [currencyOptions, setCurrencyOptions] = useState([])
-  const [fromCurrency, setFromCurrency] = useState()//referent to the 1st input box
-  const [toCurrency, setToCurrency] = useState()//referent to the 2nd input box
-  const [exchangeRate, setExchangeRate] = useState()//makes the convertion with the proper exchange rate
-  const [amount, setAmount] = useState(1)//define the default amount on the input box to 1
-
-  //set the amount changed on the 1st box(from)
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
+  const [exchangeRate, setExchangeRate] = useState()
+  const [amount, setAmount] = useState(1)
   const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
 
-  //if the input selected come for the from amount if will executed, if it comes from to amount else will be executed
   let toAmount, fromAmount
   if (amountInFromCurrency) {
     fromAmount = amount
@@ -36,38 +21,18 @@ function App() {
     fromAmount = amount / exchangeRate
   }
 
-  /**the first parameter is a function and the second parameter is an array, and whenever the items in
-   * that array change the useEffect will be reruned.
-   * it will be only called once, in a empty array, since the empty array will never change
-   * this useEffect wil only get called the first time the application loads.
-   * the BASE_URL is fetched, then the fetch get back to convert the response to JSON, wich returns
-   * another promise and this has the data.
-   */
   useEffect(() => {
     fetch(BASE_URL)
     .then(res => res.json())
     .then(data => {
-      /**setCurrencyOptions where the data is converted into an array of options.
-       * the first value in the array will be the data, and the second value is going to be all of
-       * those keys from the object in the rates part of the data.
-       * ... will destructure the array
-       */
-
-      //it will get the key of the very 1st element
       const FirstCurrency = Object.keys(data.rates)[0]
-
       setCurrencyOptions([data.base, ...Object.keys(data.rates)])
-
-      //it will get the currency from the provided API base
       setFromCurrency(data.base)
       setToCurrency(FirstCurrency)
-      setExchangeRate(data.rates[FirstCurrency]) //the rate for currency
+      setExchangeRate(data.rates[FirstCurrency])
     })
   }, [])
 
-  /*[fromCurrency or toCurrency] update the conversion as soon as currency changes withe function inside useEffect.
-  the if statement solves the problem when the use effect has ben called when from and to currency where actually
-  undefined*/
   useEffect(() => {
     if (fromCurrency != null && toCurrency != null) {
       fetch(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
@@ -76,31 +41,22 @@ function App() {
     }
   }, [fromCurrency, toCurrency])
 
-  //for changes maded on from amount(first input box) seted as trues
   function handleFromAmountChange(e) {
     setAmount(e.target.value)
     setAmountInFromCurrency(true)
   }
 
-  //for changes maded on to amount(second input box) seted as false
   function handleToAmountChange(e) {
     setAmount(e.target.value)
     setAmountInFromCurrency(false)
   }
 
   return (
-    /*the elements (h1 and the imported <CurrencyRow>) should be wrapped inside of fragments
-    otherwise it won't render.
-    */
     <>
       <h1>Convert</h1>
-
       <CurrencyRow
-        /**currencyOptions will populate all the currency options in the CurrencyRow, passing the currency
-         * optiokn as a prop(currencyOptions={currencyOptions})
-         */
         currencyOptions={currencyOptions}
-        selectedCurrency={fromCurrency}//it will pass in the selected currency
+        selectedCurrency={fromCurrency}
         onChangeCurrency={e => setFromCurrency(e.target.value)}
         onChangeAmount={handleFromAmountChange}
         amount={fromAmount}
@@ -116,5 +72,4 @@ function App() {
     </>
   );
 }
-
 export default App;
--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,32 +1,15 @@
 import React from 'react'
 
-/**the variable porps is a special keyword, which stands for properties, and is used for passing data from
- * one component to another in a uni-directional flow(one way from parent to child) in this case from
- * App.js in the CurrencyRow dropdown list to CurrencyRow.js to destructure that into object.
-*/
 export default function CurrencyRow(props) {
 
   const {
-    currencyOptions,/**currencyOptions is the very first object to be destructed trough the props variable */
+    currencyOptions,
     selectedCurrency,
-    onChangeCurrency, /**is where the function is pushed, then taked from the props. Ant time the 
-    onChange the passed value onChangeCurrency it will be updated*/
+    onChangeCurrency,
     onChangeAmount,
     amount
   } = props
 
-  /**once I have the above currencyOptions its possible to start loop through those inside of the select
-   * this {currencyOptions.map( its going to translate the array of currencyOptions into actual options.
-   * inside of the (option => function its return each one of the options, and the value for this options
-   * is just going to be the option text.
-   * as its presented a list of objects is necessary use key={option} that sets a unique value for each
-   * one of the children.
-   * 
-   * the select value should be equal to the selectedCurrency.
-   * 
-   * to update the values any time a update happens its necessary to hook up on change event for each one
-   * of the different currency selector.
-  */
   return (
     <div>
       <input type="number" className="input" value={amount} onChange={onChangeAmount}/>
